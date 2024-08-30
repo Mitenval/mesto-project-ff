f12859889cf70234b4e9982289d6394164d3e433
Mitenval <digor31@gmail.com>
2024-08-30 16:34:13 +0700

    LongDay

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,49 @@
 // @todo: Темплейт карточки
 
+
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
- function newCard(cardInfo) {
+
+function createNewCard(cardInfo) {
+
     const template = document.getElementById('card-template').content.cloneNode(true);
 
     const imgEl = template.querySelector('.card__image');
     imgEl.setAttribute('src', cardInfo.link);
 
     const titleEl = template.querySelector('.card__title');
-    titleEl.textContent = (cardInfo.name);
+    titleEl.textContent = cardInfo.name;
 
-
-    const deleteButton = template.querySelector('.card__delete-button')
+    const deleteButton = template.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', function (){
-        deleteCard(template)
-    })
+        const cardEl = deleteButton.closest('.card');
+        deleteCard(cardEl);
+    });
 
-     const list = document.querySelector('.places__list');
-     list.append(template)
- }
+    const list = document.querySelector('.places__list');
+    list.append(template.querySelector('.card'));
+}
 
 // @todo: Функция удаления карточки
-   function deleteCard(cardEl) {
-    console.log(document.querySelector('.card-template'));
-    cardEl.remove();
-   }
+
+function deleteCard(cardEl) {
+    if (cardEl) {
+        cardEl.remove();
+    } else {
+        console.error('Card element does not exist');
+    }
+}
+
 // @todo: Вывести карточки на страницу
 
-
-
 initialCards.forEach(function (card) {
-    newCard(card);
+    createNewCard(card);
 })
+
+/*
+fuck
+ */
 
 
