fc8e6c129a7adf6a3da7d3ccb3caf7ef197bcded
Mitenval <digor31@gmail.com>
2024-11-10 19:19:31 +0700

    api 1

--- src/api.js
+++ src/api.js
@@ -10,6 +10,52 @@ export const sendZapros = () => {
         });
 }
 
+function fetchUserInfo() {
+    return fetch('https://nomoreparties.co/v1/wff-cohort-24/users/me', {
+        method: 'GET',
+        headers: {
+            authorization: '3fda53f6-cae9-47b1-9be0-ea783f14a9a7'
+        }
+    })
+        .then(res => {
+            if (!res.ok) {
+                return Promise.reject(`Ошибка: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((userInfo) => {
+            console.log('Информация о пользователе:', userInfo);
+        })
+        .catch((error) => {
+            console.error('Ошибка загрузки информации о пользователе:', error);
+        });
+}
+
+fetchUserInfo();
+
+function fetchCards() {
+    return fetch('https://nomoreparties.co/v1/wff-cohort-24/cards', {
+        method: 'GET',
+        headers: {
+            authorization: '3fda53f6-cae9-47b1-9be0-ea783f14a9a7'
+        }
+    })
+        .then(res => {
+            if (!res.ok) {
+                return Promise.reject(`Ошибка: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((cards) => {
+            console.log('Карточки:', cards);
+        })
+        .catch((error) => {
+            console.error('Ошибка загрузки карточек:', error);
+        });
+}
+
+fetchCards();
+
 document.forms
 //todo для регулярного выражения
 //
